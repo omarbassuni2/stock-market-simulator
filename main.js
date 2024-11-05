b71cbc28fd4f676f4a301d7fca488c1c7d1c079c
Omar Bassuni <omarbassuni2@gmail.com>
2024-11-06 01:56:18 +0200

    feat: adding limiter on stocks received

--- main.js
+++ main.js
@@ -4,14 +4,21 @@ import StockPriceEmulator from "./StockPriceEmulator.js";
 class Main {
     constructor() {
         // DEMO CODE: create a list of 100 stock
-        const H = 1
-        const winston = new Winston(H)
-        const stocks = new Array(100)
+        const M = 20, X = 1, H = 1
+        this.winston = new Winston(H)
+        this.stocks = new Array(100)
             .fill(null)
             .map((_, index) => new StockPriceEmulator("stock " + index, 100));
-
-        // console log everytime you get a new price;
-        stocks.forEach((stock) => stock.on("tick", (m) => winston.logUpdate(JSON.stringify(m)) ));
+        this.listenWithLimit(X)
+    }
+    listenWithLimit(x) {
+        const t = Math.floor(Math.random() * (4 * x - x + 1) + x) * 1000;
+        new Promise((resolve) => {
+            setTimeout(() => {
+                this.stocks.forEach((stock) => stock.once("tick", (m) => this.winston.logUpdate(JSON.stringify(m)) ));
+                resolve();
+            }, t);
+        }).then(() => this.listenWithLimit(x));
     }
 }
 
