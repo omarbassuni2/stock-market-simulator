db64b73973af1c53cff8029a2d619a83f7a5f184
Omar Bassuni <omarbassuni2@gmail.com>
2024-11-07 20:39:53 +0200

    feat: top stocks and prettier

--- logger.js
+++ logger.js
@@ -1,52 +1,75 @@
 import winston from "winston";
 class Winston {
-    constructor(H) {
-        this.H = H
-        this.logger = winston.createLogger({
-            level: 'info',
-            format: winston.format.combine(
-                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-                winston.format.simple()
+  constructor(H) {
+    this.H = H;
+    this.logger = winston.createLogger({
+      level: "info",
+      format: winston.format.combine(
+        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        winston.format.simple()
+      ),
+      transports: [],
+    });
+    this.createAndAddNewFileTransport();
+    this.intervalId = setInterval(() => {
+      this.createAndAddNewFileTransport();
+    }, 1000 * 60 * 60 * this.H);
+    this.logUpdate = this.logUpdate.bind(this);
+  }
+  createAndAddNewFileTransport() {
+    const date = new Date()
+      .toLocaleDateString("pt-BR", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      })
+      .split("/")
+      .reverse()
+      .join("-");
+    const time = new Date().toLocaleTimeString("pt-BR", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    const filename =
+      process.env.NODE_ENV !== "test"
+        ? `./logs/${date} ${time}.log`
+        : `./logs/tests-generated/test-${date} ${time}.log`;
+    if (this.hasTransport) {
+      this.hasTransport = false;
+      this.logger.clear();
+    }
+    this.logger.add(
+      new winston.transports.File({
+        maxFiles: 5,
+        level: "info",
+        filename,
+        maxsize: 10242880,
+        format: winston.format.combine(
+          winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss.SSS" }),
+          winston.format.printf(({ timestamp, level, message }) => {
+            return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+          })
         ),
-            transports: [],
-          });
-          this.createAndAddNewFileTransport()
-          this.intervalId = setInterval(() => {
-                this.createAndAddNewFileTransport();
-            }, 1000 * 60 * this.H);
-          this.logUpdate = this.logUpdate.bind(this);
-    }
-    createAndAddNewFileTransport() {
-        const date = new Date().toLocaleDateString('pt-BR',{ year: 'numeric', month: '2-digit', day: '2-digit'}).split( '/' ).reverse( ).join( '-' );
-        const time = new Date().toLocaleTimeString('pt-BR',{ hour: '2-digit', minute: '2-digit' });
-        const filename = process.env.NODE_ENV !== 'test' ? `./logs/${date} ${time}.log` : `./logs/tests-generated/test-${date} ${time}.log`;
-        if(this.hasTransport) {
-            this.hasTransport = false
-            this.logger.clear()
-        }
-        this.logger.add(new winston.transports.File({
-            maxFiles: 5,
-            level: 'info',
-            filename,
-            maxsize: 10242880,
-            format: winston.format.combine(
-                winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
-                winston.format.printf(({ timestamp, level, message }) => {
-                    return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
-                })
-            ),
-        }))
-        this.hasTransport = true
-    }
-    logUpdate(message) {
-        this.logger.log({ level: 'info', message });
-    }
-    getLogger() {
-        return this.logger;
-    }
-    clearInterval() {
-        clearInterval(this.intervalId);
-    }
+      })
+    );
+    this.hasTransport = true;
+  }
+  logUpdate(message) {
+    this.logger.log({ level: "info", message });
+  }
+  getLogger() {
+    return this.logger;
+  }
+  clearInterval() {
+    clearInterval(this.intervalId);
+  }
 }
+let instance;
 
-export default Winston;
+function getOrCreateLogger(H) {
+  if (instance) return instance;
+  return (instance = new Winston(H));
+}
+export default (H = 1) => {
+  return getOrCreateLogger(H);
+};
--- main.js
+++ main.js
@@ -1,25 +1,13 @@
-import Winston from "./logger.js";
-import StockPriceEmulator from "./StockPriceEmulator.js";
+import getLogger from "./logger.js";
+import StockStore from "./StockStore.js";
 
 class Main {
-    constructor() {
-        // DEMO CODE: create a list of 100 stock
-        const M = 20, X = 1, H = 1
-        this.winston = new Winston(H)
-        this.stocks = new Array(100)
-            .fill(null)
-            .map((_, index) => new StockPriceEmulator("stock " + index, 100));
-        this.listenWithLimit(X)
-    }
-    listenWithLimit(x) {
-        const t = Math.floor(Math.random() * (4 * x - x + 1) + x) * 1000;
-        new Promise((resolve) => {
-            setTimeout(() => {
-                this.stocks.forEach((stock) => stock.once("tick", (m) => this.winston.logUpdate(JSON.stringify(m)) ));
-                resolve();
-            }, t);
-        }).then(() => this.listenWithLimit(x));
-    }
+  constructor(M, X, H) {
+    getLogger(H);
+    this.stockStore = new StockStore(M, X);
+    console.log("File logger and Stock Store were initialized!");
+    this.stockStore.start();
+  }
 }
 
-new Main();
+new Main(20, 1, 1);
